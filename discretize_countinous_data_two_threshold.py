b639194b4d17494b4ced5185f607787fce8cc601
Rory Ward <43640553+ruairiseosamh@users.noreply.github.com>
2020-12-06 19:28:53 +0000

    Add files via upload

---
+++ discretize_countinous_data_two_threshold.py
@@ -0,0 +1,164 @@
+# import relevant modules
+import pandas as pd
+import numpy as np
+import collections
+import math
+
+
+def get_global_entropy(y):
+        # Get probability
+        entropy = 0
+        frequencies = collections.Counter(y.values.flatten())
+        for key in frequencies:
+            frequencies[key] /= len(y)
+            entropy = entropy - (frequencies[key] * math.log2(frequencies[key]))
+        
+        # print(str(entropy))
+        return entropy
+
+def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_entropy, target):
+    p_i_less_than_smaller = 0
+    p_i_between = 0
+    p_i_greater_than_larger = 0
+    p_i_less_than_smaller_true = 0
+    p_i_between_true = 0
+    p_i_greater_than_larger_true = 0
+    p_i_less_than_smaller_false = 0
+    p_i_between_false = 0
+    p_i_greater_than_larger_false = 0
+    gain = 0
+
+    for _, row in my_dataset.iterrows():
+        if(row['colour'] <= my_first_threshold):
+            if(row['style']==target):
+                p_i_less_than_smaller_true = p_i_less_than_smaller_true + 1
+            else:
+                p_i_less_than_smaller_false = p_i_less_than_smaller_false + 1
+
+            p_i_less_than_smaller = p_i_less_than_smaller + 1
+
+        elif(row['colour'] >= my_first_threshold and row['colour'] <= my_second_threshold):
+            if(row['style']==target):
+                p_i_between_true = p_i_between_true + 1
+            else:
+                p_i_between_false = p_i_between_false + 1
+            p_i_between = p_i_between + 1
+
+        else:
+            if(row['style']==target):
+                p_i_greater_than_larger_true = p_i_greater_than_larger_true + 1
+            else:
+                p_i_greater_than_larger_false = p_i_greater_than_larger_false + 1
+            p_i_greater_than_larger = p_i_greater_than_larger + 1
+
+    try:
+        p_i_less_than_smaller_true = p_i_less_than_smaller_true/p_i_less_than_smaller
+    except:
+        p_i_less_than_smaller_true = 0
+
+    try:
+        p_i_between_true = p_i_between_true/p_i_between
+    except:
+        p_i_between_true = 0
+
+    try:
+        p_i_greater_than_larger_true = p_i_greater_than_larger_true/p_i_greater_than_larger
+    except:
+        p_i_greater_than_larger_true = 0
+    
+    try:
+        p_i_less_than_smaller_false = p_i_less_than_smaller_false/p_i_less_than_smaller
+    except:
+        p_i_less_than_smaller_false = 0
+
+    try:
+        p_i_between_false = p_i_between_false/p_i_between
+    except:
+        p_i_between_false = 0
+
+    try:
+        p_i_greater_than_larger_false = p_i_greater_than_larger_false/p_i_greater_than_larger
+    except:
+        p_i_greater_than_larger_false = 0
+
+    #print("less Than = " + str(p_i_less_than))
+    #print("Greater Than = " + str(p_i_greater_than))
+    #print("Len rows = " + str(len(my_dataset)))
+
+    try:
+        #entropy = (-1*p_i_less_than_smaller*math.log2(p_i_less_than_smaller) - (p_i_between*math.log2(p_i_between))- (p_i_greater_than_larger*math.log2(p_i_greater_than_larger)))
+        entropy_below = -p_i_less_than_smaller_true * math.log2(p_i_less_than_smaller_true)
+    except:
+        entropy_below = 0
+
+    try:
+        entropy_between = -p_i_between_true * math.log2(p_i_between_true)
+    except:
+        entropy_between  = 0
+
+    try:
+        entropy_above =  -p_i_greater_than_larger_true * math.log2(p_i_greater_than_larger_true)
+    except:
+        entropy_above = 0
+
+    below_weighting = p_i_less_than_smaller/len(my_dataset)
+    between_weighting = p_i_between/len(my_dataset)
+    above_weighting = p_i_greater_than_larger/len(my_dataset)
+
+    gain = global_entropy - (below_weighting * entropy_below) - (between_weighting * entropy_between) - (above_weighting * entropy_above)
+
+    #print("entropy = " + str(entropy))
+    
+    return gain
+
+# read in dataset
+column_headings = ["calorific_value", "nitrogen", "turbidity", "style", "alcohol", "sugars", "bitterness", "beer_id", "colour", "degree_of_fermentation"]
+df = pd.read_csv('beer.txt', sep = '\t', header=None,names=column_headings)
+
+# split dataset into attribute and outputs
+# X = pd.DataFrame(df,columns=["colour"])
+y = pd.DataFrame(df,columns=["style"])
+
+pairs =  pd.DataFrame(df,columns=["colour", "style"])
+# print(X.head())
+# print(y.head())
+sorted = pairs.sort_values('colour', ascending=True)
+#print(sorted.head())
+#print(sorted.tail())
+highest_gain = 0
+
+global_entropy = get_global_entropy(y)
+best_targets = []
+targets = np.unique(y)
+highest_overall_gain = 0
+#target = "ale"
+for target in targets:
+    highest_gain = 0
+    best_threshold_x = 0
+    best_threshold_z = 0
+
+    for x in np.arange(math.ceil(sorted.head(1)['colour'].values), math.floor(sorted.tail(1)['colour'].values), 0.1):
+        for z in np.arange(math.ceil(sorted.head(1)['colour'].values), math.floor(sorted.tail(1)['colour'].values), 0.1):
+            #if(x<y):
+            resulting_gain = calculate_gain(sorted, x, z, global_entropy, target)
+            if resulting_gain > highest_gain:
+                highest_gain = resulting_gain
+                best_threshold_x = x
+                best_threshold_z = z
+
+    print("highest information gain for " + target  + " achieved is " + str(highest_gain))
+    print("best thresholds are " + str(best_threshold_x) + " " + str(best_threshold_z))
+
+    if(highest_gain > highest_overall_gain):
+        highest_overall_gain = highest_gain
+        #highest_overall_gains =  highest_overall_gain + highest_overall_gains 
+        best_targets = target.append(best_targets)
+    else:
+        best_targets =  best_targets.append(target))
+
+print("Best overall gain is " + str(highest_overall_gain))
+print("Ordered targets are " + best_targets)
+
+# print("looping boundaries are " + str(math.ceil(sorted.head(1)['colour'].values)) + " " + str(math.floor(sorted.tail(1)['colour'].values)))
+
+# print(sorted)
