8b56200fe399876d9530caf74f994fe68b941393
Rory Ward <43640553+ruairiseosamh@users.noreply.github.com>
2020-12-07 19:39:12 +0000

    Update discretize_countinous_data_two_threshold.py

--- discretize_countinous_data_two_threshold.py
+++ discretize_countinous_data_two_threshold.py
@@ -16,7 +16,7 @@ def get_global_entropy(y):
         # print(str(entropy))
         return entropy
 
-def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_entropy, target):
+def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_entropy, target, column_heading):
     p_i_less_than_smaller = 0
     p_i_between = 0
     p_i_greater_than_larger = 0
@@ -29,7 +29,7 @@ def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_e
     gain = 0
 
     for _, row in my_dataset.iterrows():
-        if(row['colour'] <= my_first_threshold):
+        if(row[column_heading] <= my_first_threshold):
             if(row['style']==target):
                 p_i_less_than_smaller_true = p_i_less_than_smaller_true + 1
             else:
@@ -37,7 +37,7 @@ def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_e
 
             p_i_less_than_smaller = p_i_less_than_smaller + 1
 
-        elif(row['colour'] >= my_first_threshold and row['colour'] <= my_second_threshold):
+        elif(row[column_heading] >= my_first_threshold and row[column_heading] <= my_second_threshold):
             if(row['style']==target):
                 p_i_between_true = p_i_between_true + 1
             else:
@@ -115,49 +115,47 @@ def calculate_gain(my_dataset, my_first_threshold, my_second_threshold, global_e
 column_headings = ["calorific_value", "nitrogen", "turbidity", "style", "alcohol", "sugars", "bitterness", "beer_id", "colour", "degree_of_fermentation"]
 df = pd.read_csv('beer.txt', sep = '\t', header=None,names=column_headings)
 
+#highest_gain = 0
+best_targets = []
+highest_overall_gain = 0
+
+y = pd.DataFrame(df,columns=["style"])
+global_entropy = get_global_entropy(y)
+targets = np.unique(y)
+
 # split dataset into attribute and outputs
 # X = pd.DataFrame(df,columns=["colour"])
-y = pd.DataFrame(df,columns=["style"])
+for column_heading in column_headings:
+    if(column_heading!="beer_id" and column_heading!="style"):
+        pairs =  pd.DataFrame(df,columns=[column_heading, "style"])
+        sorted = pairs.sort_values(column_heading, ascending=True)
 
-pairs =  pd.DataFrame(df,columns=["colour", "style"])
-# print(X.head())
-# print(y.head())
-sorted = pairs.sort_values('colour', ascending=True)
-#print(sorted.head())
-#print(sorted.tail())
-highest_gain = 0
+        for target in targets:
+            highest_gain = 0
+            best_threshold_x = 0
+            best_threshold_z = 0
 
-global_entropy = get_global_entropy(y)
-best_targets = []
-targets = np.unique(y)
-highest_overall_gain = 0
-#target = "ale"
-for target in targets:
-    highest_gain = 0
-    best_threshold_x = 0
-    best_threshold_z = 0
+            for x in np.arange(sorted.head(1)[column_heading].values, sorted.tail(1)[column_heading].values, 0.1):
+                for z in np.arange(sorted.head(1)[column_heading].values, sorted.tail(1)[column_heading].values, 0.1):
+                    #if(x<y):
+                    resulting_gain = calculate_gain(sorted, x, z, global_entropy, target, column_heading)
+                    if resulting_gain > highest_gain:
+                        highest_gain = resulting_gain
+                        best_threshold_x = x
+                        best_threshold_z = z
 
-    for x in np.arange(math.ceil(sorted.head(1)['colour'].values), math.floor(sorted.tail(1)['colour'].values), 0.1):
-        for z in np.arange(math.ceil(sorted.head(1)['colour'].values), math.floor(sorted.tail(1)['colour'].values), 0.1):
-            #if(x<y):
-            resulting_gain = calculate_gain(sorted, x, z, global_entropy, target)
-            if resulting_gain > highest_gain:
-                highest_gain = resulting_gain
-                best_threshold_x = x
-                best_threshold_z = z
+            print("highest information gain for " + target + " in " + column_heading + " achieved is " + str(highest_gain))
+            print("best thresholds are " + str(best_threshold_x) + " " + str(best_threshold_z))
 
-    print("highest information gain for " + target  + " achieved is " + str(highest_gain))
-    print("best thresholds are " + str(best_threshold_x) + " " + str(best_threshold_z))
-
-    if(highest_gain > highest_overall_gain):
-        highest_overall_gain = highest_gain
-        #highest_overall_gains =  highest_overall_gain + highest_overall_gains 
-        best_targets = target.append(best_targets)
-    else:
-        best_targets =  best_targets.append(target))
+            if(highest_gain > highest_overall_gain):
+                highest_overall_gain = highest_gain
+                #highest_overall_gains =  highest_overall_gain + highest_overall_gains 
+                #best_targets = target.append(best_targets)
+            #else:
+                #best_targets =  best_targets.append(target)
 
 print("Best overall gain is " + str(highest_overall_gain))
-print("Ordered targets are " + best_targets)
+        #print("Ordered targets are " + best_targets)
 
 # print("looping boundaries are " + str(math.ceil(sorted.head(1)['colour'].values)) + " " + str(math.floor(sorted.tail(1)['colour'].values)))
 
